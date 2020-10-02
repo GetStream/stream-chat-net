d48ded5bb1ad365e8aa35f95d113accc7e70e3e2
Vishal Narkhede <vishalnarkhede.iitd@gmail.com>
2020-10-02 15:50:29 +0200

    Adding system message support for addMembers and removeMembers (#15)

--- src/stream-chat-net/IChannel.cs
+++ src/stream-chat-net/IChannel.cs
@@ -26,8 +26,8 @@ namespace StreamChat
         Task Delete();
         Task Truncate();
 
-        Task AddMembers(IEnumerable<string> userIDs);
-        Task RemoveMembers(IEnumerable<string> userIDs);
+        Task AddMembers(IEnumerable<string> userIDs, MessageInput msg = null);
+        Task RemoveMembers(IEnumerable<string> userIDs, MessageInput msg = null);
         Task AddModerators(IEnumerable<string> userIDs);
         Task DemoteModerators(IEnumerable<string> userIDs);
 
