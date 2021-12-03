1cfc776aac058ba0dbb57a9e0dd6c3f0f3ea1c55
peterdeme <peter.deme@getstream.io>
2021-12-03 14:05:58 +0100

    Lets test stuff

--- scripts/create_release_branch.sh
+++ scripts/create_release_branch.sh
@@ -3,27 +3,20 @@
 VERSION=$1
 echo "Preparing release $VERSION"
 
-# Splitting the version: 0.1.2 -> an array of [0, 1, 2]
-IFS=. read -r -a splitted <<< "$VERSION"
-
-# Update VERSION file
-echo """
-MAJOR=${splitted[0]}
-MINOR=${splitted[1]}
-BUG=${splitted[2]}
-""" > VERSION
-
 # Update .csproj file
 # This regex switches all occurences of >0.21.0< to >0.22.0<
 # So <PackageVersion>0.21.0</PackageVersion> becomes <PackageVersion>0.22.0</PackageVersion>
 sed -i -r "s/>[0-9]+\.[0-9]+\.[0-9]+</>$VERSION</g" src/stream-chat-net/stream-chat-net.csproj
 
-# Create changelog, plus commit
+# Create changelog, plus commit all changes
 # commmit-all: Commit all staged changes (csproj, VERSION), not just files affected by standard-version
 # Tagging will done by the GitHub release step, so skip it
+git branch "release-$VERSION"
 git config --global user.name 'GH Actions' 
 git config --global user.email 'release@getstream.io'
 git add .
-npx --yes standard-version --release-as "$VERSION" --skip.tag --commit-all
+npx --yes standard-version@9.3.2 --release-as "$VERSION" --skip.tag --commit-all
+current_branch=$(git symbolic-ref --short HEAD)
+git push -u origin "$current_branch"
 
 echo "Done!"
