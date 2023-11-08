429417d2c369bcd4fd4a2fcfa2de9f72d9ed5ef1
Daniel Sierpiński <daniel.sierpinski@getstream.io>
2023-11-08 12:16:13 +0100

    Add EventResponse model suitable for deserializing events e.g. from webhooks (#136)
    * Add EventResponse model suitable for deserializing events e.g. from webhooks
    
    * Fix code style

--- tests/EventClientTests.cs
+++ tests/EventClientTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Newtonsoft.Json;
 using NUnit.Framework;
 using StreamChat.Models;
 
@@ -44,6 +45,48 @@ namespace StreamChatTests
             resp.Event.Type.Should().BeEquivalentTo(expectedEvent.Type);
             resp.Event.User.Id.Should().BeEquivalentTo(_user.Id);
             resp.Event.GetData<int[]>("foo")[0].Should().Be(expectedEvent.GetData<int[]>("foo")[0]);
+        }
+
+        [Test]
+        public void TestDeserializingReceivedEventAsync()
+        {
+            var messageDeletedEventJson = @"
+{
+  ""cid"": ""messaging:fun"",
+  ""type"": ""message.deleted"",
+  ""message"": {
+    ""id"": ""268d121f-82e0-4de1-8c8b-ef1201efd7a3"",
+    ""text"": ""new stuff"",
+    ""html"": ""<p>new stuff</p>\n"",
+    ""type"": ""deleted"",
+    ""user"": {
+      ""id"": ""76cd8430-2f91-4059-90e5-02dffb910297"",
+      ""role"": ""user"",
+      ""created_at"": ""2019-04-24T09:44:21.390868Z"",
+      ""updated_at"": ""2019-04-24T09:44:22.537305Z"",
+      ""last_active"": ""2019-04-24T09:44:22.535872Z"",
+      ""online"": false
+    },
+    ""attachments"": [],
+    ""latest_reactions"": [],
+    ""own_reactions"": [],
+    ""reaction_counts"": {},
+    ""reply_count"": 0,
+    ""created_at"": ""2019-04-24T09:44:22.57073Z"",
+    ""updated_at"": ""2019-04-24T09:44:22.717078Z"",
+    ""deleted_at"": ""2019-04-24T09:44:22.730524Z"",
+    ""mentioned_users"": []
+  },
+  ""created_at"": ""2019-04-24T09:44:22.733305Z"",
+  ""request_info"": {
+    ""type"": ""client"",
+    ""ip"": ""86.84.2.2"",
+    ""user_agent"": ""Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/117.0"",
+    ""sdk"": ""stream-chat-react-10.11.0-stream-chat-javascript-client-browser-8.12.1""
+  }
+}";
+
+            var deserializeObject = JsonConvert.DeserializeObject<EventResponse>(messageDeletedEventJson);
         }
 
         [Test]
