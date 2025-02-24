6352d668a800e8eec7f6d26d4222aa3b9f51e514
Daniel Sierpiński <33436839+sierpinskid@users.noreply.github.com>
2025-02-24 14:28:15 +0100

    split code example

--- samples/DocsExamples/ChannelExporting.cs
+++ samples/DocsExamples/ChannelExporting.cs
@@ -21,14 +21,19 @@ internal class ChannelExporting
     public async Task ExportUsersAsync()
     {
         var exportResponse = await _userClient.ExportUsersAsync(new[] { "user-id-1", "user-id-2" });
-        var taskId = exportResponse.TaskId; // Returns async task ID for the export operation
+        var taskId = exportResponse.TaskId;
+    }
 
-        // You can use the ITaskClient to check the status of the export operation
+    public async Task RetrievingTaskStatusAsync()
+    {
+        var taskId = string.Empty;
+        
+        // ITaskClient can provide the status of the export operation
         var taskStatus = await _taskClient.GetTaskStatusAsync(taskId);
         if (taskStatus.Status == AsyncTaskStatus.Completed)
         {
             // The export operation is completed
-            // You can obtain the export file URL from the Result object
+            // Result object contains the export file URL
             var exportedFileUrl = taskStatus.Result.Values.First().ToString();
         }
     }
