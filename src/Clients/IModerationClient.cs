d08cb77acea88733da9bbbacf1dd9766119ca83f
Vishal Narkhede <vishalnarkhede.iitd@gmail.com>
2025-03-27 13:24:37 +0100

    feat: moderation apis

---
+++ src/Clients/IModerationClient.cs
@@ -0,0 +1,37 @@
+using System.Threading.Tasks;
+using StreamChat.Models;
+
+namespace StreamChat.Clients
+{
+    /// <summary>
+    /// A client that can be used to access moderation endpoints of a Stream Chat application.
+    /// </summary>
+    public interface IModerationClient
+    {
+        /// <summary>
+        /// Check content for moderation.
+        /// </summary>
+        /// <param name="entityType">Type of entity to be checked E.g., stream:user, stream:chat:v1:message, or any custom string</param>
+        /// <param name="entityId">ID of the entity to be checked. This is mainly for tracking purposes</param>
+        /// <param name="entityCreatorId">ID of the entity creator</param>
+        /// <param name="moderationPayload">Content to be checked for moderation. E.g., texts, images, videos</param>
+        /// <param name="configKey">Configuration key for moderation</param>
+        /// <param name="options">Additional options for moderation check</param>
+        Task<ModerationCheckResponse> CheckAsync(
+            string entityType,
+            string entityId,
+            string entityCreatorId,
+            ModerationPayload moderationPayload,
+            string configKey,
+            ModerationCheckOptions options = null);
+
+        /// <summary>
+        /// Experimental: Check user profile for moderation.
+        /// This will not create any review queue items for the user profile.
+        /// You can use this to check whether to allow certain user profile to be created or not.
+        /// </summary>
+        /// <param name="userId">ID of the user</param>
+        /// <param name="profile">User profile data containing username and/or profile image</param>
+        Task<ModerationCheckResponse> CheckUserProfileAsync(string userId, UserProfileCheckRequest profile);
+    }
+} 
---
+++ src/Clients/ModerationClient.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using StreamChat.Models;
+using StreamChat.Rest;
+
+namespace StreamChat.Clients
+{
+    public class ModerationClient : ClientBase, IModerationClient
+    {
+        internal ModerationClient(IRestClient client) : base(client)
+        {
+        }
+
+        public async Task<ModerationCheckResponse> CheckAsync(
+            string entityType,
+            string entityId,
+            string entityCreatorId,
+            ModerationPayload moderationPayload,
+            string configKey,
+            ModerationCheckOptions options = null)
+        {
+            var request = new
+            {
+                entity_type = entityType,
+                entity_id = entityId,
+                entity_creator_id = entityCreatorId,
+                moderation_payload = moderationPayload,
+                config_key = configKey,
+                options
+            };
+
+            return await ExecuteRequestAsync<ModerationCheckResponse>(
+                "api/v2/moderation/check",
+                HttpMethod.POST,
+                HttpStatusCode.OK,
+                request);
+        }
+
+        public async Task<ModerationCheckResponse> CheckUserProfileAsync(string userId, UserProfileCheckRequest profile)
+        {
+            if (string.IsNullOrEmpty(profile?.Username) && string.IsNullOrEmpty(profile?.Image))
+            {
+                throw new ArgumentException("Either username or image must be provided", nameof(profile));
+            }
+
+            var payload = new ModerationPayload
+            {
+                Texts = !string.IsNullOrEmpty(profile.Username) ? new[] { profile.Username }.ToList() : null,
+                Images = !string.IsNullOrEmpty(profile.Image) ? new[] { profile.Image }.ToList() : null
+            };
+
+            return await CheckAsync(
+                ModerationEntityTypes.UserProfile,
+                userId,
+                userId,
+                payload,
+                "user_profile:default",
+                new ModerationCheckOptions
+                {
+                    ForceSync = true,
+                    TestMode = true
+                });
+        }
+    }
+} 
