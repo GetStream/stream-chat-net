6b94302de09b9a6a6edf7994e0f344d36092f80a
javierdfm <javierdfm@gmail.com>
2025-09-01 13:39:19 +0200

    [CHA-1211] Added Member to message response (#180)
    * Added Member message response and test
    
    * Added test
    
    * Updated test
    
    * Attempt
    
    * Updated test
    
    * Not needed
    
    * Removed optional
    
    * Try delete channel

---
+++ tests/MemberMessageResponseTests.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+using StreamChat.Models;
+
+namespace StreamChatTests;
+
+internal class MemberMessageResponseTests : TestBase
+{
+    private UserRequest _user1;
+    private UserRequest _user2;
+    private ChannelWithConfig _channel;
+
+    [SetUp]
+    public async Task SetupAsync()
+    {
+        (_user1, _user2) = (await UpsertNewUserAsync(), await UpsertNewUserAsync());
+
+        var channelResponse = await _channelClient.GetOrCreateAsync(
+            "messaging",
+            Guid.NewGuid().ToString(),
+            new ChannelGetRequest
+            {
+                Data = new ChannelRequest
+                {
+                    Members = new[]
+                    {
+                        new ChannelMember { UserId = _user1.Id },
+                        new ChannelMember { UserId = _user2.Id, ChannelRole = "custom_role" },
+                    },
+                    CreatedBy = new UserRequest { Id = _user1.Id },
+                },
+            });
+
+        _channel = channelResponse.Channel;
+    }
+
+    [TearDown]
+    public async Task TearDownAsync()
+    {
+        await TryDeleteUsersAsync(_user1.Id, _user2.Id);
+        await TryDeleteChannelAsync(_channel.Cid);
+    }
+
+    [Test]
+    public async Task WhenSendingMessagesExpectChannelRoleIncludedAsync()
+    {
+        var user1MessageResp = await _messageClient.SendMessageAsync(_channel.Type, _channel.Id, _user1.Id, "Hello from user1");
+        user1MessageResp.Message.Member.Should().NotBeNull();
+        user1MessageResp.Message.Member.ChannelRole.Should().Be("channel_member");
+
+        var user2MessageResp = await _messageClient.SendMessageAsync(_channel.Type, _channel.Id, _user2.Id, "Hello from user2");
+        user2MessageResp.Message.Member.Should().NotBeNull();
+        user2MessageResp.Message.Member.ChannelRole.Should().Be("custom_role");
+
+        var channelState = await _channelClient.GetOrCreateAsync(_channel.Type, _channel.Id, new ChannelGetRequest { State = true, Watch = false });
+
+        var fetchedUser1Msg = channelState.Messages.First(m => m.Id == user1MessageResp.Message.Id);
+        fetchedUser1Msg.Member.Should().NotBeNull();
+        fetchedUser1Msg.Member.ChannelRole.Should().Be(user1MessageResp.Message.Member.ChannelRole);
+
+        var fetchedUser2Msg = channelState.Messages.First(m => m.Id == user2MessageResp.Message.Id);
+        fetchedUser2Msg.Member.Should().NotBeNull();
+        fetchedUser2Msg.Member.ChannelRole.Should().Be("custom_role");
+    }
+}
